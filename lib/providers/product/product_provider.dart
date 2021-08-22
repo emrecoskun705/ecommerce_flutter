c5acaf342916a586076ccdb299559a5301498d0b
emrecoskun705 <emrecoskun705@gmail.com>
2021-08-22 12:44:10 +0300

    Update product detail with change notifier provider

--- lib/providers/product/product_provider.dart
+++ lib/providers/product/product_provider.dart
@@ -1,12 +1,40 @@
 import 'package:ecommerce_flutter/models/product.dart';
+import 'package:ecommerce_flutter/services/product/favourite_product_api.dart';
+import 'package:ecommerce_flutter/services/product/product_detail_api.dart';
 import 'package:flutter/material.dart';
 
 class ProductProvider with ChangeNotifier {
   Product? product;
+  bool isLoading = true;
 
-  ProductProvider();
+  Future fetchProduct(int productId) async {
+    setIsLoading(true);
+    var product = await ProductDetailApi().fetchData(productId);
+    setProduct(product);
+    setIsLoading(false);
+  }
+
+  Future postFavourite() async {
+    await FavouriteProductApi().postData(
+        product!.id,
+        product!.isFavourite
+            ? FavouriteProductAction.remove
+            : FavouriteProductAction.add);
+    setFavourite(!product!.isFavourite);
+  }
+
+  void setIsLoading(bool value) {
+    this.isLoading = value;
+    notifyListeners();
+  }
+
   void setProduct(Product product) {
     this.product = product;
+    notifyListeners();
+  }
+
+  void setFavourite(bool value) {
+    product!.isFavourite = value;
     notifyListeners();
   }
 }
--- lib/screens/details/product_detail.dart
+++ lib/screens/details/product_detail.dart
@@ -1,10 +1,8 @@
-import 'package:ecommerce_flutter/models/product.dart';
 import 'package:ecommerce_flutter/providers/UserProvider.dart';
+import 'package:ecommerce_flutter/providers/product/product_provider.dart';
 import 'package:ecommerce_flutter/screens/components/rounded_button.dart';
 import 'package:ecommerce_flutter/screens/details/components/product_description.dart';
 import 'package:ecommerce_flutter/screens/details/components/product_image_carousel.dart';
-import 'package:ecommerce_flutter/services/product/favourite_product_api.dart';
-import 'package:ecommerce_flutter/services/product/product_detail_api.dart';
 import 'package:ecommerce_flutter/size_config.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -21,111 +19,94 @@ class ProductDetail extends StatefulWidget {
 }
 
 class _ProductDetailState extends State<ProductDetail> {
-  Future<Product>? futureProduct;
+  ProductProvider productProvider = ProductProvider();
 
   @override
   void initState() {
+    productProvider.fetchProduct(widget.productId);
     super.initState();
-
-    futureProduct = ProductDetailApi().fetchData(widget.productId);
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Product>(
-      future: futureProduct,
-      builder: (BuildContext context, AsyncSnapshot<Product> snapshot) {
-        // if connection state is not done it will show a loading screen
-        if (snapshot.connectionState != ConnectionState.done) {
-          return ModalProgressHUD(
-            inAsyncCall: true,
-            child: Container(
-              color: Colors.white,
-            ),
-          );
-        }
-        if (snapshot.hasError) {
-          return Text('error');
-        }
-        // when api fetches the future and if has data, product detail screen will appear
-        if (snapshot.hasData) {
-          void _postFavourite() async {
-            await FavouriteProductApi().postData(
-                snapshot.data!.id,
-                snapshot.data!.isFavourite
-                    ? FavouriteProductAction.remove
-                    : FavouriteProductAction.add);
-          }
-
-          return Scaffold(
-            appBar: AppBar(
-              iconTheme: IconThemeData(color: Colors.black),
-              title: Text(
-                snapshot.data!.title,
-                style: TextStyle(color: Colors.black.withOpacity(0.7)),
+    return ChangeNotifierProvider.value(
+      value: productProvider,
+      child: Consumer<ProductProvider>(
+        builder: (context, data, _) {
+          if (productProvider.isLoading) {
+            return ModalProgressHUD(
+              inAsyncCall: true,
+              child: Container(
+                color: Colors.white,
               ),
-              actions: [
-                Provider.of<UserProvider>(context).isLoggedIn
-                    ? IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _postFavourite();
-                            snapshot.data!.isFavourite =
-                                !snapshot.data!.isFavourite;
-                          });
-                        },
-                        icon: Icon(
-                          CupertinoIcons.heart_fill,
-                          color: snapshot.data!.isFavourite
-                              ? Colors.red
-                              : Colors.black.withOpacity(0.3),
-                        ),
+            );
+          } else {
+            // when api fetches the future and if it is not loading, product detail screen will appear
+            return Scaffold(
+              appBar: AppBar(
+                iconTheme: IconThemeData(color: Colors.black),
+                title: Text(
+                  data.product!.title,
+                  style: TextStyle(color: Colors.black.withOpacity(0.7)),
+                ),
+                actions: [
+                  Provider.of<UserProvider>(context).isLoggedIn
+                      ? IconButton(
+                          onPressed: () {
+                            data.postFavourite();
+                          },
+                          icon: Icon(
+                            CupertinoIcons.heart_fill,
+                            color: data.product!.isFavourite
+                                ? Colors.red
+                                : Colors.black.withOpacity(0.3),
+                          ),
+                        )
+                      : SizedBox(),
+                ],
+                backgroundColor: Color(0xFFFFF),
+                elevation: 0,
+              ),
+              body: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: getProportionateScreenHeight(15),
+                      vertical: getProportionateScreenWidth(10)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        data.product!.title,
+                        style: TextStyle(
+                            fontSize: getProportionateScreenWidth(14)),
+                      ),
+                      SizedBox(
+                        height: getProportionateScreenHeight(10),
+                      ),
+                      ProductImageCarousel(
+                        product: data.product!,
+                      ),
+                      SizedBox(
+                        height: getProportionateScreenHeight(14),
+                      ),
+                      ProductDescription(
+                        price: data.product!.price,
+                        discountPrice: data.product!.discountPrice,
+                        description: data.product!.description,
+                      ),
+                      RoundedButton(
+                        colour: Colors.lightBlueAccent,
+                        title: 'Add to cart',
+                        onPressed: () {},
                       )
-                    : SizedBox(),
-              ],
-              backgroundColor: Color(0xFFFFF),
-              elevation: 0,
-            ),
-            body: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: getProportionateScreenHeight(15),
-                    vertical: getProportionateScreenWidth(10)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      snapshot.data!.title,
-                      style:
-                          TextStyle(fontSize: getProportionateScreenWidth(14)),
-                    ),
-                    SizedBox(
-                      height: getProportionateScreenHeight(10),
-                    ),
-                    ProductImageCarousel(
-                      product: snapshot.data!,
-                    ),
-                    SizedBox(
-                      height: getProportionateScreenHeight(14),
-                    ),
-                    ProductDescription(
-                      price: snapshot.data!.price,
-                      discountPrice: snapshot.data!.discountPrice,
-                      description: snapshot.data!.description,
-                    ),
-                    RoundedButton(
-                      colour: Colors.lightBlueAccent,
-                      title: 'Add to cart',
-                      onPressed: () {},
-                    )
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        }
-        return Text('some other error');
-      },
+            );
+          }
+        },
+      ),
     );
   }
 }

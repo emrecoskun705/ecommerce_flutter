bf3693aa10c7becdeca067a5c7c19936bd801d48
emrecoskun705 <emrecoskun705@gmail.com>
2021-08-28 19:54:38 +0300

    Add form and validators to login screen

--- lib/screens/account/components/login_screen.dart
+++ lib/screens/account/components/login_screen.dart
@@ -12,6 +12,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
@@ -32,56 +33,74 @@ class _LoginScreenState extends State<LoginScreen> {
     return ModalProgressHUD(
       inAsyncCall: Provider.of<UserProvider>(context).isLoading,
       child: SingleChildScrollView(
-        child: Container(
-          padding:
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 30.0),
-            child: Column(
-              children: [
-                Text(
-                  'Log In',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25.0,
-                      color: Color(0xFFFB8500)),
-                ),
-                SizedBox(height: 20.0),
-                TextField(
-                  controller: emailController,
-                  enabled: !Provider.of<UserProvider>(context).isLoading,
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: kAccountInputDecoration.copyWith(
-                    labelText: 'E-mail',
-                    errorText:
-                        Provider.of<UserProvider>(context).isError ? '' : null,
+        child: Form(
+          key: _formKey,
+          child: Container(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 30.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Log In',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0,
+                        color: Color(0xFFFB8500)),
                   ),
-                ),
-                SizedBox(height: 20.0),
-                TextField(
-                  controller: passwordController,
-                  enabled: !Provider.of<UserProvider>(context).isLoading,
-                  textAlign: TextAlign.center,
-                  obscureText: true,
-                  decoration: kAccountInputDecoration.copyWith(
-                    labelText: 'Password',
-                    errorText: Provider.of<UserProvider>(context).isError
-                        ? Provider.of<UserProvider>(context).getMessage()
-                        : null,
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    controller: emailController,
+                    validator: (val) {
+                      if (val == '') {
+                        return 'This field is required';
+                      }
+                      return null;
+                    },
+                    enabled: !Provider.of<UserProvider>(context).isLoading,
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: kAccountInputDecoration.copyWith(
+                      labelText: 'E-mail',
+                      errorText: Provider.of<UserProvider>(context).isError
+                          ? ''
+                          : null,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                RoundedButton(
-                  colour: Color(0xFFFFB703),
-                  title: 'Log In',
-                  onPressed: () {
-                    _login();
-                  },
-                ),
-              ],
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    controller: passwordController,
+                    validator: (val) {
+                      if (val == '') {
+                        return 'This field is required';
+                      }
+                      return null;
+                    },
+                    enabled: !Provider.of<UserProvider>(context).isLoading,
+                    textAlign: TextAlign.center,
+                    obscureText: true,
+                    decoration: kAccountInputDecoration.copyWith(
+                      labelText: 'Password',
+                      errorText: Provider.of<UserProvider>(context).isError
+                          ? Provider.of<UserProvider>(context).getMessage()
+                          : null,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  RoundedButton(
+                    colour: Color(0xFFFFB703),
+                    title: 'Log In',
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _login();
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),

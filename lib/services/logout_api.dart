108826faf34e93c90218990409ce4230843d700d
emrecoskun705 <emrecoskun705@gmail.com>
2021-09-18 21:59:20 +0300

    fix logout_api.dart logout api was working wrong it did not delete the token in server.

--- lib/services/logout_api.dart
+++ lib/services/logout_api.dart
@@ -1,6 +1,6 @@
 import 'package:ecommerce_flutter/constants.dart';
 import 'package:http/http.dart' as http;
-
+import 'dart:io';
 // Logout api is used for changing User token
 // when we call this api user token changes
 
@@ -11,10 +11,9 @@ class LogoutApi {
 
   Future<dynamic> fetchData() async {
     var url = Uri.parse('$kServerAuthApiURL/logout/');
-    http.Response response = await http.post(
-      url,
-      body: {'token': 'Token $token'},
-    );
+    http.Response response = await http.post(url, headers: {
+      HttpHeaders.authorizationHeader: 'Token ${token.toString()}'
+    });
     return response;
   }
 }
